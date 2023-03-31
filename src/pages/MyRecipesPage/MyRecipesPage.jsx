2fb544945a129407c0d74e54817bb368967118e0
Ruslan Tendora <ruslantendora@gmail.com>
2023-04-01 00:27:01 +0300

    Update MyRecipesPage.jsx

--- src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import MyRecipesList from 'components/MyRecipesList/MyRecipesList';
+import MyRecipesItem from 'components/MyRecipesList/MyRecipesItem/MyRecipesItem';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
 
 const MyRecipesPage = () => {
   return (
-    <div>
+    <>
+      <Header />
+      <MyRecipesList>
+        <MyRecipesItem></MyRecipesItem>
+        <MyRecipesItem></MyRecipesItem>
+        <MyRecipesItem></MyRecipesItem>
+        <MyRecipesItem></MyRecipesItem>
+      </MyRecipesList>
+      <Footer />
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default MyRecipesPage
+export default MyRecipesPage;
