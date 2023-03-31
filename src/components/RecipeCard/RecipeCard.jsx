1c296fb4653cdaa691064929b65ecc98c15d65df
Hurduz Oksana <hurduzoksana@gmail.com>
2023-03-31 21:13:26 +0300

    added RecipeCard logic

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-
-// import card from './cardrecipe.jpg';
+import { Link } from 'react-router-dom';
 import { Card, RecipePicture, RecipeTitle } from './RecipeCard.styled';
+import NotFoundImg from '../../images/NotFound/noimage.jpg';
 
-export const RecipeCard = () => {
+export const RecipeCard = ({ data }) => {
   return (
     <Card>
-      {/* <RecipePicture src={card} alt="" /> */}
-      <RecipeTitle>Banana Pankakes</RecipeTitle>
+      <Link to={`/recipe/${data._id}`}>
+        <RecipePicture
+          src={data.preview ? data.preview : { NotFoundImg }}
+          alt={data.title}
+        />
+        <RecipeTitle data-id={data._id}>{data.title}</RecipeTitle>
+      </Link>
     </Card>
   );
 };
