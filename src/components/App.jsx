1eb28a4c147795cdeca97f27ff884af4097a3292
Anna Tsepilova <virgo.tsav@gmail.com>
2023-03-31 12:22:22 +0300

    start adding app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,48 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { lazy, Suspense } from 'react';
 import Loader from './Loader/Loader';
 
+import { SharedLayout } from 'components/SharedLayout/SharedLayout';
+import PublicRoute from 'components/PublicRoute/PublicRoute';
+
 // const RegisterForm = lazy(() => import('./RegisterForm/RegisterForm'));
-const NotFound = lazy(() => import('./NotFound/NotFound'));
+const WelcomePage = lazy(() => import('pages/WelcomePage/WelcomePage'));
+const SignInPage = lazy(() => import('pages/SignInPage/SignInPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+
+const AddRecipePage = lazy(() => import('pagesAddRecipePage/AddRecipePage'));
+const CategoriesPage = lazy(() =>
+  import('pages/CategoriesPage/CategoriesPage')
+);
+const FavoritesPage = lazy(() => import('pages/FavoritesPage/FavoritesPage'));
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const MyRecipesPage = lazy(() => import('pages/MyRecipesPage/MyRecipesPage'));
+const RecipePage = lazy(() => import('pages/RecipePage/RecipePage'));
+const SearchPage = lazy(() => import('pages/SearchPage/SearchPage'));
+const ShoppingListPage = lazy(() =>
+  import('pages/ShoppingListPage/ShoppingListPage')
+);
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 
 export const App = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        {/* <Route path="/register" element={<RegisterForm />} />
+    // <BrowserRouter basename="yummy-app-front">
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* <Route path="/register" element={<RegisterForm />} />
         <Route path="/signin" element={<RegisterForm />} /> */}
-        <Route path="/" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
+          <Route path="/" element={<PublicRoute />}>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/" element={<SignInPage />} />
+            <Route path="/" element={<RegisterPage />} />
+          </Route>
+          <Route path="/" element={<SharedLayout />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 };
