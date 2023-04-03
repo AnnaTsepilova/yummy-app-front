20b1a419709783a7cc71c0d0ad6c0435cc6fc8f3
Hurduz Oksana <hurduzoksana@gmail.com>
2023-04-03 14:14:33 +0300

    changed styles

--- src/pages/CategoriesCard/CategoriesCard.jsx
+++ src/pages/CategoriesCard/CategoriesCard.jsx
@@ -2,7 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
-import { RecipeCard } from 'components/RecipeCard/RecipeCard';
+import { RecipeCard } from 'components/Main/PreviewCategories/RecipeCard';
+
 import { Table } from './CategoriesCard.styled';
 import { getLimitedRecipes } from 'redux/commonRecipes/commonSelectors';
 import { getLimitedRecipesByCategory } from 'redux/commonRecipes/commonOperations';
@@ -26,7 +27,14 @@ const CategoriesCard = () => {
       {limitedRecipes
         ? limitedRecipes
             .slice(0, 8)
-            .map((data, i) => <RecipeCard data={data} key={i} />)
+            .map(({ description, preview, title, _id }) => (
+              <RecipeCard
+                id={_id}
+                image={preview}
+                altText={description}
+                text={title}
+              />
+            ))
         : null}
     </Table>
   );
--- src/pages/CategoriesCard/CategoriesCard.styled.jsx
+++ src/pages/CategoriesCard/CategoriesCard.styled.jsx
@@ -1,20 +1,23 @@
 import styled from 'styled-components';
 
-export const Table = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
+export const Table = styled.div`
+  display: grid;
+  gap: 14px;
+  margin: 0 auto;
   margin-bottom: 100px;
   margin-top: 32px;
-  gap: 28px;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: 1fr;
 
   @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
     gap: 32px;
     margin-bottom: 200px;
     margin-top: 50px;
   }
 
   @media (min-width: 1440px) {
+    grid-template-columns: repeat(4, 1fr);
     gap: 14px;
     row-gap: 100px;
   }
