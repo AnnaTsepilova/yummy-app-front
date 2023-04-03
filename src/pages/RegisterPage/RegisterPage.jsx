75c7164e1aba9219895d832f453e9aef44238628
efirnet <efirnet@gmail.com>
2023-04-03 22:11:36 +0300

    add form login/register

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import AuthContent from 'components/AuthContent/AuthContent';
-// import RegisterForm from 'components/RegisterForm/RegisterForm';
+import RegisterForm from 'components/RegisterForm/RegisterForm';
 // import { AuthLink } from 'components/AuthContent/AuthContent.styled';
 
 const RegisterPage = () => {
   return (
-    <AuthContent>
-      {/* <RegisterForm /> */}
-      {/* <AuthLink to="/signin">Sign In</AuthLink> */}
-    </AuthContent>
+    <>
+      <RegisterForm />
+      <AuthContent />
+    </>
   );
 };
 
