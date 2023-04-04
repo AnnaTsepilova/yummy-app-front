cc34f7eda3d6a56d3a5b85c5f7af0e4a8e46ac51
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-05 00:11:23 +0300

    add bg-leaves

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+import LeavesBgLeftMob from 'images/LeavesOnMainBottom/leaves-on-main-bottom-mob-cut@1x.png';
+import LeavesBgLeftMob2 from 'images/LeavesOnMainBottom/leaves-on-main-bottom-mob-cut@2x.png';
+import LeavesBgLeftTab from 'images/LeavesOnMainBottom/leaves-on-main-bottom-tab-cut@1x.png';
+import LeavesBgLeftTab2 from 'images/LeavesOnMainBottom/leaves-on-main-bottom-tab-cut@2x.png';
+import LeavesBgLeftDesc from 'images/LeavesOnMainBottom/leaves-on-main-bottom-desc-cut@1x.png';
+import LeavesBgLeftDesc2 from 'images/LeavesOnMainBottom/leaves-on-main-bottom-desc-cut@2x.png';
+
 export const Container = styled.div`
   min-width: 320px;
   padding: 18px 16px 0 16px;
@@ -14,5 +21,46 @@ export const Container = styled.div`
     width: 1440px;
     padding-left: 100px;
     padding-right: 100px;
+  }
+`;
+
+export const FooterBgLeaves = styled.div`
+  min-width: 320px;
+  background-image: url('${LeavesBgLeftMob}');
+  /* width: 256px; */
+  height: 125px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: left;
+  z-index: -1;
+
+  @media screen and (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url('${LeavesBgLeftMob2}');
+  }
+
+  @media screen and (min-width: 768px) {
+    background-image: url('${LeavesBgLeftTab}');
+    /* height: 646px; */
+    height: 260px;
+
+    @media screen and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url('${LeavesBgLeftTab2}');
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    background-image: url('${LeavesBgLeftDesc}');
+    /* height: 852px; */
+    height: 280px;
+
+    @media screen and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url('${LeavesBgLeftDesc2}');
+    }
   }
 `;
