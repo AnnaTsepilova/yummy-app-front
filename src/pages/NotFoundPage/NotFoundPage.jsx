d6b4e1308cdf5974576f88d66324cdefead83686
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-05 09:34:16 +0300

    initial

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { NotFoundSvg } from 'pages/NotFoundPage/NotFoundPage.styled';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <>
+      <NotFoundSvg />
+      <p>We are sorry, </p>
+      <p>but the page you were looking for can't be found..</p>
+    </>
+  );
 };
 
 export default NotFoundPage;
