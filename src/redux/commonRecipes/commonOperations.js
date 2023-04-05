09de32aaadd6d470749c459f29c2a8ed0ce88637
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-05 16:02:05 +0300

    add fix

--- src/redux/commonRecipes/commonOperations.js
+++ src/redux/commonRecipes/commonOperations.js
@@ -4,25 +4,12 @@ import {
   getCategoryListAPI,
   getLimitedRecipesByCategoryAPI,
   getAllRecipesByCategoryAPI,
-  // getRecipesByQueryAPI,
+  getRecipesByQueryAPI,
 } from 'service/API/dishesApi';
-
-import axios from 'axios';
-
-const setToken = token => {
-  if (token) {
-    axios.defaults.headers.common.authorization = `Bearer ${token}`;
-  } else {
-    delete axios.defaults.headers.common.authorization;
-  }
-};
 
 export const getCategoryList = createAsyncThunk(
   'commonRecipes/categoryList',
   async (_, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ1MzJjY2M5M2ExNTRlZWQ3OGQ2NSIsImlhdCI6MTY4MDY5MjAzOCwiZXhwIjoxNjgxMDUyMDM4fQ.WQy4eJg73FBrTJ0b786cxoeWCx9QXS8-pb0il5gXjig'
-    );
     try {
       const data = await getCategoryListAPI();
       return data;
@@ -35,9 +22,6 @@ export const getCategoryList = createAsyncThunk(
 export const getLimitedRecipesByCategory = createAsyncThunk(
   'outerRecipes/limitedRecipes',
   async (params, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ1MzJjY2M5M2ExNTRlZWQ3OGQ2NSIsImlhdCI6MTY4MDY5MjAzOCwiZXhwIjoxNjgxMDUyMDM4fQ.WQy4eJg73FBrTJ0b786cxoeWCx9QXS8-pb0il5gXjig'
-    );
     try {
       const { category, limit } = params;
       const data = await getLimitedRecipesByCategoryAPI(category, limit);
@@ -51,9 +35,6 @@ export const getLimitedRecipesByCategory = createAsyncThunk(
 export const getAllRecipesByCategory = createAsyncThunk(
   'outerRecipes/allRecipes',
   async (category, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ1MzJjY2M5M2ExNTRlZWQ3OGQ2NSIsImlhdCI6MTY4MDY5MjAzOCwiZXhwIjoxNjgxMDUyMDM4fQ.WQy4eJg73FBrTJ0b786cxoeWCx9QXS8-pb0il5gXjig'
-    );
     try {
       const data = await getAllRecipesByCategoryAPI(category);
       return data;
@@ -63,52 +44,15 @@ export const getAllRecipesByCategory = createAsyncThunk(
   }
 );
 
-// export const getCategoryList = createAsyncThunk(
-//   'commonRecipes/categoryList',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const data = await getCategoryListAPI();
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
-
-// export const getLimitedRecipesByCategory = createAsyncThunk(
-//   'outerRecipes/limitedRecipes',
-//   async (params, { rejectWithValue }) => {
-//     try {
-//       const { category, limit } = params;
-//       const data = await getLimitedRecipesByCategoryAPI(category, limit);
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
-
-// export const getAllRecipesByCategory = createAsyncThunk(
-//   'outerRecipes/allRecipes',
-//   async (category, { rejectWithValue }) => {
-//     try {
-//       const data = await getAllRecipesByCategoryAPI(category);
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
-
-// export const getRecipesByQuery = createAsyncThunk(
-//   'outerRecipes/recipesByQuery',
-//   async (params, { rejectWithValue }) => {
-//     try {
-//       const { query, page, per_page } = params;
-//       const data = await getRecipesByQueryAPI(query, page, per_page);
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
+export const getRecipesByQuery = createAsyncThunk(
+  'outerRecipes/recipesByQuery',
+  async (params, { rejectWithValue }) => {
+    try {
+      const { query, page, per_page } = params;
+      const data = await getRecipesByQueryAPI(query, page, per_page);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
