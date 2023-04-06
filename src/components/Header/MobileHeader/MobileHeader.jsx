cf807ccb9f4c2d41d2ed92cd41946c0875d9a073
Ruslan Tendora <ruslantendora@gmail.com>
2023-04-06 16:12:51 +0300

    add modal with user profile

--- src/components/Header/MobileHeader/MobileHeader.jsx
+++ src/components/Header/MobileHeader/MobileHeader.jsx
@@ -6,17 +6,40 @@ import {
   UserAvatarWrp,
   UserWithoutAvatar,
   UserName,
+  UserInform,
+  PopUp,
+  EditWrapper,
+  EditBtn,
+  EditText,
+  LogOutBtn,
+  ModalContentWrp,
+  CLoseBtn,
+  CloseIcon,
 } from './MobileHeader.styled';
 import { ReactComponent as MobileLogo } from 'images/Logo/logo-header.svg';
 import { ReactComponent as BurgerIcon } from 'images/HeaderMenu/header-menu.svg';
+import { ReactComponent as EditIcon } from 'images/EditProfilePen/edit-profile-pen.svg';
+import { ReactComponent as ErrowRight } from 'images/UserMenu/arrow-right.svg';
 import { useState } from 'react';
 import MobileNavMenu from '../MobileNavMenu/MobileNavMenu';
+import { Modal } from '@mui/material';
+import UserProfileForm from 'components/UserProfileForm/UserProfileForm';
 
 const MobileHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [popUpIsOpen, setPopUpIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openNav = () => {
     setIsOpen(!isOpen);
+  };
+
+  const openPopUp = () => {
+    setPopUpIsOpen(!popUpIsOpen);
+  };
+
+  const togleModal = () => {
+    setModalIsOpen(!modalIsOpen);
   };
 
   return (
@@ -27,17 +50,43 @@ const MobileHeader = () => {
           <MobileLogo />
         </Link>
         <UserWrp>
-          <UserAvatarWrp>
-            <UserWithoutAvatar />
-            {/* <img src={NotFoundAvatar} alt="avatar" /> */}
-          </UserAvatarWrp>
+          <UserInform onClick={openPopUp}>
+            <UserAvatarWrp>
+              <UserWithoutAvatar />
+              {/* <img src={NotFoundAvatar} alt="avatar" /> */}
+            </UserAvatarWrp>
 
-          <UserName>Name</UserName>
+            <UserName>Name</UserName>
+            {popUpIsOpen && (
+              <PopUp>
+                <EditWrapper>
+                  <EditText>Edit profile</EditText>
+                  <EditBtn type="button" onClick={togleModal}>
+                    <EditIcon />
+                  </EditBtn>
+                </EditWrapper>
+                <LogOutBtn>
+                  Log out
+                  <ErrowRight />
+                </LogOutBtn>
+              </PopUp>
+            )}
+          </UserInform>
           <BurgerBtn onClick={openNav}>
             <BurgerIcon />
           </BurgerBtn>
         </UserWrp>
       </MobileHeaderWrp>
+      {modalIsOpen && (
+        <Modal open={modalIsOpen} onClose={togleModal}>
+          <ModalContentWrp>
+            <CLoseBtn onClick={togleModal}>
+              <CloseIcon />
+            </CLoseBtn>
+            <UserProfileForm />
+          </ModalContentWrp>
+        </Modal>
+      )}
     </>
   );
 };
