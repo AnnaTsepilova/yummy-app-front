70f2e17dc02f84a6138ae5414e40261ed85b3088
Hurduz Oksana <hurduzoksana@gmail.com>
2023-04-06 09:36:09 +0300

    added token.set for commonOperations

--- src/redux/commonRecipes/commonOperations.js
+++ src/redux/commonRecipes/commonOperations.js
@@ -2,11 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 import {
-  // getCategoryListAPI,
+  getCategoryListAPI,
   getLimitedRecipesByCategoryAPI,
   getAllRecipesByCategoryAPI,
   getRecipesByQueryAPI,
-  getRecipesByIngredientAPI,
+  // getRecipesByIngredientAPI,
 } from 'service/API/dishesApi';
 
 const token = {
@@ -20,38 +20,27 @@ const token = {
 
 export const getCategoryList = createAsyncThunk(
   'commonRecipes/categoryList',
-  async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    token.set(state.auth.accessToken);
+  async (_, { rejectWithValue, getState }) => {
+    const state = getState();
+    const persistedAccessToken = state.auth.accessToken;
+    if (!persistedAccessToken) {
+      return rejectWithValue();
+    }
+    token.set(persistedAccessToken);
     try {
-      const { data } = await axios.get('/recipes/category-list');
+      const data = await getCategoryListAPI();
       return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.status);
+      return rejectWithValue(error.response.status);
     }
   }
 );
-
-// export const getCategoryList = createAsyncThunk(
-//   'commonRecipes/categoryList',
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const data = await getCategoryListAPI();
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
 
 export const getMainPage = createAsyncThunk(
   'commonRecipes/mainPage',
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
     token.set(state.auth.accessToken);
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ4YTE2NjU2NTMxYzYzZDdiOTc2NyIsInNpZCI6IjY0MmQ5YzIyZDA2NTA1NzhkYjEzZTNiMyIsImlhdCI6MTY4MDcxMDY5MCwiZXhwIjoxNjgwNzE0MjkwfQ.n0ZTeHXnHHTCXJJebTbX_oSs8oq2CCrLdgdADj_MRqs'
-    );
     try {
       const { data } = await axios.get('/recipes/main-page');
       return data;
@@ -63,10 +52,13 @@ export const getMainPage = createAsyncThunk(
 
 export const getLimitedRecipesByCategory = createAsyncThunk(
   'outerRecipes/limitedRecipes',
-  async (params, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ4YTE2NjU2NTMxYzYzZDdiOTc2NyIsInNpZCI6IjY0MmQ5YzIyZDA2NTA1NzhkYjEzZTNiMyIsImlhdCI6MTY4MDcxMDY5MCwiZXhwIjoxNjgwNzE0MjkwfQ.n0ZTeHXnHHTCXJJebTbX_oSs8oq2CCrLdgdADj_MRqs'
-    );
+  async (params, { rejectWithValue, getState }) => {
+    const state = getState();
+    const persistedAccessToken = state.auth.accessToken;
+    if (!persistedAccessToken) {
+      return rejectWithValue();
+    }
+    token.set(persistedAccessToken);
     try {
       const { category, limit } = params;
       const data = await getLimitedRecipesByCategoryAPI(category, limit);
@@ -76,12 +68,16 @@ export const getLimitedRecipesByCategory = createAsyncThunk(
     }
   }
 );
+
 export const getAllRecipesByCategory = createAsyncThunk(
   'outerRecipes/allRecipes',
-  async (category, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ4YTE2NjU2NTMxYzYzZDdiOTc2NyIsInNpZCI6IjY0MmQ5YzIyZDA2NTA1NzhkYjEzZTNiMyIsImlhdCI6MTY4MDcxMDY5MCwiZXhwIjoxNjgwNzE0MjkwfQ.n0ZTeHXnHHTCXJJebTbX_oSs8oq2CCrLdgdADj_MRqs'
-    );
+  async (category, { rejectWithValue, getState }) => {
+    const state = getState();
+    const persistedAccessToken = state.auth.accessToken;
+    if (!persistedAccessToken) {
+      return rejectWithValue();
+    }
+    token.set(persistedAccessToken);
     try {
       const data = await getAllRecipesByCategoryAPI(category);
       return data;
@@ -93,45 +89,19 @@ export const getAllRecipesByCategory = createAsyncThunk(
 
 export const getRecipesByQuery = createAsyncThunk(
   'outerRecipes/recipesByQuery',
-  async (params, { rejectWithValue }) => {
-    setToken(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ4YTE2NjU2NTMxYzYzZDdiOTc2NyIsInNpZCI6IjY0MmQ5YzIyZDA2NTA1NzhkYjEzZTNiMyIsImlhdCI6MTY4MDcxMDY5MCwiZXhwIjoxNjgwNzE0MjkwfQ.n0ZTeHXnHHTCXJJebTbX_oSs8oq2CCrLdgdADj_MRqs'
-    );
+  async (params, { rejectWithValue, getState }) => {
+    const state = getState();
+    const persistedAccessToken = state.auth.accessToken;
+    if (!persistedAccessToken) {
+      return rejectWithValue();
+    }
+    token.set(persistedAccessToken);
     try {
-      const { query } = params;
-      const data = await getRecipesByQueryAPI({ value: query, flags: '' });
+      const { query, page, per_page } = params;
+      const data = await getRecipesByQueryAPI(query, page, per_page);
       return data;
     } catch (error) {
       return rejectWithValue(error.response.status);
     }
   }
 );
-
-// export const getRecipesByQuery = createAsyncThunk(
-//   'outerRecipes/recipesByQuery',
-//   async (params, { rejectWithValue }) => {
-//     setToken(
-//       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmQ4YTE2NjU2NTMxYzYzZDdiOTc2NyIsInNpZCI6IjY0MmQ4YTJjNjU2NTMxYzYzZDdiOTc2YSIsImlhdCI6MTY4MDcwNjA5MiwiZXhwIjoxNjgwNzA5NjkyfQ.IxuBF3Pm_Q-IZv9g6EdYplkNFvIYgasXbgT-zDi2Zkc'
-//     );
-//     try {
-//       const { query } = params;
-//       const data = await getRecipesByQueryAPI(query);
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
-
-// export const getRecipesByIngredient = createAsyncThunk(
-//   'ingredients/recipes',
-//   async (params, { rejectWithValue }) => {
-//     try {
-//       const { ingredient } = params;
-//       const data = await getRecipesByIngredientAPI(ingredient);
-//       return data;
-//     } catch (error) {
-//       return rejectWithValue(error.response.status);
-//     }
-//   }
-// );
--- src/service/API/dishesApi.js
+++ src/service/API/dishesApi.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://y-3wt8.onrender.com/api';
 
-// export const getCategoryListAPI = () => {
-//   return axios.get('/recipes/category-list').then(({ data }) => {
-//     return data;
-//   });
-// };
+export const getCategoryListAPI = () => {
+  return axios.get('/recipes/category-list').then(({ data }) => {
+    return data;
+  });
+};
 
 export const getLimitedRecipesByCategoryAPI = category => {
   return axios.get(`/recipes/${category}`).then(({ data }) => {
