0c23556542345e85dfbcd3c47a8a2e3482a68240
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-06 11:43:46 +0300

    change deploy - dev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,8 +53,6 @@ export const App = () => {
   // }, [dispatch]);
 
   useEffect(() => {
-    console.log('App', accessToken);
-    console.log('App', location.pathname);
     if (accessToken) {
       authOperations.setAuthHeader(accessToken);
       dispatch(setAccessToken(accessToken));
