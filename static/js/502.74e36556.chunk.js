70401644816757385918a8c59e19a4da652364c6
AnnaTsepilova <virgo.tsav@gmail.com>
2023-04-07 15:39:19 +0000

    Deploying to gh-pages from @ AnnaTsepilova/yummy-app-front@41d907c3e426b1eff684a16ceb14e89574145ac0 🚀

--- static/js/502.74e36556.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyummy_app_front=self.webpackChunkyummy_app_front||[]).push([[502],{2502:function(u,n,t){t.r(n);t(2791);var e=t(184);n.default=function(){return(0,e.jsx)("div",{})}}}]);
-//# sourceMappingURL=502.74e36556.chunk.js.map
