73adbaa3c266c0fff29194582bea634b50230436
efirnet <efirnet@gmail.com>
2023-04-07 23:51:58 +0300

    fix signin

--- src/components/AuthForm/ValidationSchema.jsx
+++ src/components/AuthForm/ValidationSchema.jsx
@@ -1,23 +1,33 @@
 import * as Yup from 'yup';
+import { useLocation } from 'react-router-dom';
 
-const validationSchema = Yup.object({
-  name: Yup.string()
-    .min(3, 'Name must be at least 3 characters long')
-    .matches(
-      /^[a-zA-Z\s-]+$/,
-      'Name can only contain letters, spaces, and dashes'
-    )
-    .required(' '),
-  email: Yup.string()
-    .email('Invalid email address')
-    .required('Email is required'),
-  password: Yup.string()
-    .min(5, 'Password must be at least 5 characters long')
-    .matches(
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/,
-      'Password must contain at least one uppercase letter, one lowercase letter, and one number'
-    )
-    .required('Enter a valid Password'),
-});
+const useValidationSchema = () => {
+  const { pathname } = useLocation();
+  const isLogin = pathname === '/signin';
 
-export default validationSchema;
+  const validationSchema = Yup.object().shape({
+    name: !isLogin
+      ? Yup.string()
+          .required(' ')
+          .min(3, 'Name must be at least 3 characters long')
+          .matches(
+            /^[a-zA-Zа-яА-ЯёЁ\s-]+$/,
+            'Name can only contain letters, spaces, and dashes'
+          )
+      : Yup.string(),
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(5, 'Password must be at least 5 characters long')
+      .matches(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/,
+        'Password must contain at least one uppercase letter, one lowercase letter, and one number'
+      )
+      .required('Enter a valid Password'),
+  });
+
+  return validationSchema;
+};
+
+export default useValidationSchema;
