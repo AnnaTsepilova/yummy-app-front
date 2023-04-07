0ac48bc27384feabb8484dcd674333c579f33b05
Oleksii Onufriienko <o.alexey1202@gmail.com>
2023-04-07 16:49:08 +0300

    add_remove Ingredients

--- src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
+++ src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { DeleteButton, InputIngredWraper, TitleWraper, FormWrapper, TitleStyled, IncDecWraper, IncDecButton, IncDecCounter, SelectIngredStyled, SelectIngredUnit } from './RecipeIngredientsFields.styled'
+import { ListIngredientsItem, DeleteButton, InputIngredWraper, TitleWraper, FormWrapper, TitleStyled, IncDecWraper, IncDecButton, IncDecCounter, SelectIngredStyled, SelectIngredUnit } from './RecipeIngredientsFields.styled'
 
-const RecipeIngredientsFields = ({dataIngredient}) => {
-  const { category} = dataIngredient; 
+const RecipeIngredientsFields = ({ingredientsList, handleIncIngredient, handleDecIngredient}) => {
+  // const { category} = dataIngredient; 
 
-  const handleOnInc = async () => {
-    console.log("+");
-  }
-  const handleOnDec = async () => { 
-     console.log("-");   
-  }
+
   const handleOnDelete = async () => { 
     console.log("Press Delete");
   }
@@ -20,28 +15,37 @@ const RecipeIngredientsFields = ({dataIngredient}) => {
         <TitleWraper>
               <TitleStyled>Ingredients</TitleStyled>
               <IncDecWraper>
-                <IncDecButton type='button'onClick={handleOnDec}>-</IncDecButton>
-                <IncDecCounter>0</IncDecCounter>
-                <IncDecButton type='button' onClick={handleOnInc}>+</IncDecButton>
+                <IncDecButton type='button'onClick={handleDecIngredient}>-</IncDecButton>
+            <IncDecCounter>{ ingredientsList.length}</IncDecCounter>
+                <IncDecButton type='button' onClick={handleIncIngredient}>+</IncDecButton>
               </IncDecWraper>
         </TitleWraper>  
-        <InputIngredWraper>
-              <SelectIngredStyled
-                // defaultValue={colourOptions}
-                options={category}
-                isSearchable={false}
-                // placeholder="Category"
-                classNamePrefix="react-select"
-              />
-              <SelectIngredUnit
-                // defaultValue={colourOptions}
-                options={category}
-                isSearchable={false}
-                // placeholder="Category"
-                classNamePrefix="react-select"
-              />
-          <DeleteButton onClick={handleOnDelete}>X</DeleteButton>
-        </InputIngredWraper>
+          <ul>
+            {ingredientsList.map((e, index) => {
+              return (
+                <ListIngredientsItem key={e.id}>
+                    <InputIngredWraper>
+                          <SelectIngredStyled
+                            // defaultValue={colourOptions}
+                            // options={category}
+                            isSearchable={false}
+                            // placeholder="Category"
+                            classNamePrefix="react-select"
+                          />
+                          <SelectIngredUnit
+                            // defaultValue={colourOptions}
+                            // options={category}
+                            isSearchable={false}
+                            // placeholder="Category"
+                            classNamePrefix="react-select"
+                          />
+                     <DeleteButton onClick={handleOnDelete}>X</DeleteButton>
+                    </InputIngredWraper>
+                </ListIngredientsItem>
+              )
+            })}
+          </ul>
+
 
       </FormWrapper>
     </>
