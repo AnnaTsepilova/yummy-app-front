7b15d9ecac92bfb48ae10e4009d39bdd79ac196a
Oleksii Onufriienko <o.alexey1202@gmail.com>
2023-04-09 02:09:12 +0300

    form

--- src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
+++ src/components/RecipeIngredientsFields/RecipeIngredientsFields.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { listUnits } from 'components/AddRecipeForm/AddRecipeForm.const';
+import {getIngredientsList} from 'service/API/dishesApi';
 import { CloseIconButton, IncIconButton, DecrIconButton,ListIngredientsItem, DeleteButton, InputIngredWraper, TitleWraper, FormWrapper, TitleStyled, IncDecWraper, IncDecButton, IncDecCounter, SelectIngredStyled, SelectIngredUnit } from './RecipeIngredientsFields.styled'
 
-const RecipeIngredientsFields = ({dataRecipe, handleIncIngredient, handleDecIngredient, handleOndeleteContact}) => {
-  const {ingredientList, userIngredientsList, listUnits } = dataRecipe;
+const RecipeIngredientsFields = ({userList, handleOnUserIngredientsList, handleIncIngredient, handleDecIngredient, handleOndeleteContact}) => {
+  const {userIngredientsList} = userList;
+  const [ingredientList, setIngredientList] = useState([]);
 
+  function initIngredientFunc(list) { 
+    const tmp = list.map(e => {
+      return ({ value: e._id, label: `${e.ttl}` })
+    });
+    setIngredientList(tmp);
+  };
+
+    useEffect(() => {
+    try {
+      getIngredientsList().then(initIngredientFunc);
+    } catch (error) {
+       Notify.failure('Error database connection!', {
+            fontSize: '16px',
+            width: '350px',
+            padding: '10px',
+          });
+    }
+
+    }, []);
+  
   return (
     <>
       <FormWrapper>
         <TitleWraper>
               <TitleStyled>Ingredients</TitleStyled>
           <IncDecWraper>
-
               <IncDecButton type='button' onClick={handleDecIngredient}>
                   <DecrIconButton/>
               </IncDecButton>
-              <IncDecCounter>{ userIngredientsList.length}</IncDecCounter>
+              <IncDecCounter>{ userList.length}</IncDecCounter>
               <IncDecButton type='button' onClick={handleIncIngredient}>
                   <IncIconButton/>
               </IncDecButton>
