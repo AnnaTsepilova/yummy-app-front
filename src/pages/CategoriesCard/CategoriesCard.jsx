982f079b959480cc4691f3b2bc1e6052a275f17a
Hurduz Oksana <hurduzoksana@gmail.com>
2023-04-09 11:38:00 +0300

    added NotFound for vegeterian category

--- src/pages/CategoriesCard/CategoriesCard.jsx
+++ src/pages/CategoriesCard/CategoriesCard.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 import { RecipeCard } from 'components/Main/PreviewCategories/RecipeCard';
-
+import NotFound from 'components/NotFound/NotFound';
 import { Table } from './CategoriesCard.styled';
 import { getLimitedRecipes } from 'redux/commonRecipes/commonSelectors';
 import { getLimitedRecipesByCategory } from 'redux/commonRecipes/commonOperations';
@@ -22,7 +22,7 @@ const CategoriesCard = () => {
 
   return isPending ? (
     <Loader />
-  ) : (
+  ) : limitedRecipes.length > 0 ? (
     <Table>
       {limitedRecipes
         ? limitedRecipes
@@ -38,6 +38,8 @@ const CategoriesCard = () => {
             ))
         : null}
     </Table>
+  ) : (
+    <NotFound text={`No results found`} />
   );
 };
 
