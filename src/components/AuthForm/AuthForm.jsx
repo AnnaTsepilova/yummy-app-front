0a14b1fab8c246609fd2fcb50189ffece08bfac6
efirnet <efirnet@gmail.com>
2023-04-10 19:00:37 +0300

    fix validation auth error-warning-correct 3.0

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -28,31 +27,24 @@ const AuthForm = () => {
   const validationSchema = useValidationSchema();
 
   const dispatch = useDispatch();
-  const [nameError, setNameError] = useState(false);
-  const [nameCorrect, setNameCorrect] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [emailCorrect, setEmailCorrect] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [passwordCorrect, setPasswordCorrect] = useState(false);
-  const [passwordStrength, setPasswordStrength] = useState(false);
-
-  const initialValues = { name: '', email: '', password: '' };
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
   const formik = useFormik({
     initialValues,
     validationSchema,
     onSubmit: async values => {
-      console.log(values);
+      const { passwordStrength, ...data } = values;
+
+      console.log(data);
       try {
         if (isLogin) {
-          await dispatch(authOperations.signIn(values));
-          // Notify.success('You have successfully signed in', {
-          //   fontSize: '16px',
-          //   width: '350px',
-          //   padding: '10px',
-          // });
+          await dispatch(authOperations.signIn(data));
         } else {
-          const result = await dispatch(authOperations.signUp(values));
+          const result = await dispatch(authOperations.signUp(data));
           if (result.type === 'auth/signup/rejected') {
             return;
           }
@@ -61,7 +53,7 @@ const AuthForm = () => {
             width: '350px',
             padding: '10px',
           });
-          await dispatch(authOperations.signIn(values));
+          await dispatch(authOperations.signIn(data));
           formik.resetForm();
         }
       } catch (error) {
@@ -79,49 +71,27 @@ const AuthForm = () => {
     },
   });
 
-  const handleInputChange = e => {
+  const handleNameChange = e => {
     formik.handleChange(e);
-    const { name, value } = e.target;
-
-    const strength = checkPasswordStrength(formik.values.password);
-    setPasswordStrength(strength);
-
-    const regex = {
-      name: /^[a-zA-Z\s-]+$/,
-      email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/,
-    };
-    const errors = {
-      name: value.length === 0 || value.length < 3,
-      email: value.length === 0 || !regex.email.test(value),
-      password: value.length === 0 || !regex.password.test(value),
-    };
-    const correct = {
-      name: !errors.name,
-      email: !errors.email,
-      password: !errors.password,
-    };
-    switch (name) {
-      case 'name':
-        setNameError(errors.name);
-        setNameCorrect(correct.name);
-        break;
-      case 'email':
-        setEmailError(errors.email);
-        setEmailCorrect(correct.email);
-        break;
-      case 'password':
-        setPasswordError(errors.password);
-        setPasswordCorrect(correct.password);
-        break;
-      default:
-        break;
-    }
+    formik.setFieldTouched('name', true);
   };
 
+  const handleEmailChange = e => {
+    formik.handleChange(e);
+    formik.setFieldTouched('email', true);
+  };
+
+  const handleInputChange = e => {
+    formik.handleChange(e);
+    const strength = checkPasswordStrength(formik.values.password);
+    formik.setFieldValue('passwordStrength', strength);
+  };
+
+  const passwordStrength = formik.values.passwordStrength;
   const showError = formik.touched.password && formik.errors.password;
   const showWarning = passwordStrength === 'weak' && !showError;
-  const showCorrect = passwordCorrect && !showError && !showWarning;
+  const showCorrect =
+    !showError && !showWarning && formik.values.passwordStrength;
 
   return (
     <FormContainer>
@@ -145,19 +115,16 @@ const AuthForm = () => {
             >
               <FormInput
                 placeholder="Name"
-                onChange={handleInputChange}
+                onChange={handleNameChange}
                 onBlur={formik.handleBlur}
                 type="text"
                 name="name"
                 autoComplete="name"
-                error={nameError}
-                correct={nameCorrect}
                 value={formik.values.name}
               />
               {formik.touched.name && formik.errors.name && (
                 <Error>{formik.errors.name}</Error>
               )}
-              {nameCorrect && <Correct></Correct>}
             </InputWrap>
           )}
           <InputWrap
@@ -173,19 +140,16 @@ const AuthForm = () => {
           >
             <FormInput
               placeholder="Email"
-              onChange={handleInputChange}
+              onChange={handleEmailChange}
               onBlur={formik.handleBlur}
               type="email"
               name="email"
               autoComplete="email"
               value={formik.values.email}
-              error={emailError}
-              correct={emailCorrect}
             />
             {formik.touched.email && formik.errors.email && (
               <Error>{formik.errors.email}</Error>
             )}
-            {emailCorrect && <Correct></Correct>}
           </InputWrap>
           <InputWrap
             iconUrl={img.iconLock}
@@ -195,15 +159,9 @@ const AuthForm = () => {
             iconError={img.iconError}
             iconWarning={img.iconWarning}
             iconCorrect={img.iconCorrect}
-            {...formik.getFieldProps('password')}
-            error={Boolean(formik.touched.password && formik.errors.password)}
-            correct={Boolean(
-              formik.touched.password && !formik.errors.password
-            )}
-            warning={
-              Boolean(formik.touched.password && !formik.errors.password) &&
-              !passwordStrength
-            }
+            error={showError}
+            warning={showWarning}
+            correct={showCorrect}
           >
             <FormInput
               placeholder="Password"
@@ -213,9 +171,6 @@ const AuthForm = () => {
               name="password"
               autoComplete="current-password"
               value={formik.values.password}
-              error={passwordError}
-              correct={passwordCorrect}
-              warning={passwordStrength}
             />
             {showError && <Error>{formik.errors.password}</Error>}
             {showWarning && !showCorrect && (
