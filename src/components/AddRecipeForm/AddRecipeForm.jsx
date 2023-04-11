a0356295c8c7e12a3c89ccaf0cfb865a50b0573c
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-11 22:07:45 +0300

    delete console.logs

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,55 +1,66 @@
 import React from 'react';
-import { useState, useEffect} from "react";
+import { useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Formik} from 'formik';
+import { Formik } from 'formik';
 import { nanoid } from 'nanoid';
 
-
-import { StyledForm, WrapperSubmitButton, WrapperPage, WrapperPopularRecipe } from './AddRecipeForm.styled';
+import {
+  StyledForm,
+  WrapperSubmitButton,
+  WrapperPage,
+  WrapperPopularRecipe,
+} from './AddRecipeForm.styled';
 import { SearchBlackBtn } from 'components/Buttons/Buttons';
 import { TitleStyled } from 'components/PopularRecipe/PopularRecipe.style';
-import RecipeDescriptionFields from 'components/RecipeDescriptionFields/RecipeDescriptionFields'
+import RecipeDescriptionFields from 'components/RecipeDescriptionFields/RecipeDescriptionFields';
 import RecipeIngredientsFields from 'components/RecipeIngredientsFields/RecipeIngredientsFields';
 import RecipePreparationFields from 'components/RecipePreparationFields/RecipePreparationFields';
-import PopularRecipe from 'components/PopularRecipe/PopularRecipe'
+import PopularRecipe from 'components/PopularRecipe/PopularRecipe';
 import { WrapperSocialLinks } from './SocialLinks/SocialLinks.styled';
 import SocialLinks from './SocialLinks/SocialLinks';
-import { getPopularRecipe, addRecipe, addRecipeImg} from 'service/API/dishesApi';
+import {
+  getPopularRecipe,
+  addRecipe,
+  addRecipeImg,
+} from 'service/API/dishesApi';
 
 const AddRecipeForm = () => {
-
   const [popularRecipeList, setPopularRecipeList] = useState([]);
   const [imgURL, setImageURL] = useState('');
   const [itemTitleRecipe, setItemTitleRecipe] = useState('');
   const [aboutRecipe, setAboutRecipe] = useState('');
   const [category, setCategory] = useState({ value: '', label: '' });
-  const [cookingTimeRecipe, setCookingTimeRecipe] = useState({ value: '', label: '' })
-  const [userIngredientsList, setUserIngredientList] = useState([{
+  const [cookingTimeRecipe, setCookingTimeRecipe] = useState({
+    value: '',
+    label: '',
+  });
+  const [userIngredientsList, setUserIngredientList] = useState([
+    {
       _id: nanoid(),
       id: '',
       name: '',
       unitCount: '',
       unit: '',
-    }]);
+    },
+  ]);
   const [recipePreparation, setRecipePreperation] = useState('');
 
   function initPopularFunc(list) {
     setPopularRecipeList(list);
-   };
+  }
 
   useEffect(() => {
     try {
       getPopularRecipe().then(initPopularFunc);
     } catch (error) {
-       Notify.failure('Error database connection!', {
-            fontSize: '16px',
-            width: '350px',
-            padding: '10px',
-          });
+      Notify.failure('Error database connection!', {
+        fontSize: '16px',
+        width: '350px',
+        padding: '10px',
+      });
     }
   }, []);
-  
-  
+
   const handleIncIngredient = () => {
     const ingredient = {
       _id: nanoid(),
@@ -57,17 +68,17 @@ const AddRecipeForm = () => {
       name: '',
       unitCount: '',
       unit: '',
-    }
+    };
     setUserIngredientList([ingredient, ...userIngredientsList]);
-  }
+  };
 
-  const handleDecIngredient = () => { 
+  const handleDecIngredient = () => {
     const newIngredientsList = [...userIngredientsList];
     newIngredientsList.shift();
     setUserIngredientList(newIngredientsList);
   };
 
-  const handleOnDeleteIngredient = (index) => {
+  const handleOnDeleteIngredient = index => {
     const newIngredientsList = [...userIngredientsList];
     newIngredientsList.splice(index, 1);
     setUserIngredientList(newIngredientsList);
@@ -96,23 +107,25 @@ const AddRecipeForm = () => {
     image: '',
     itemTitleRecipe: '',
     aboutRecipe: '',
-  }
+  };
 
   const resetForm = () => {
     setImageURL('');
     setItemTitleRecipe('');
     setAboutRecipe('');
     setCategory({ value: '', label: '' });
-    setCookingTimeRecipe({ value: '', label: '' })
-    setUserIngredientList([{
-      _id: nanoid(),
-      id: '',
-      name: '',
-      unitCount: '',
-      unit: '',
-    }]);
+    setCookingTimeRecipe({ value: '', label: '' });
+    setUserIngredientList([
+      {
+        _id: nanoid(),
+        id: '',
+        name: '',
+        unitCount: '',
+        unit: '',
+      },
+    ]);
     setRecipePreperation('');
-  }
+  };
 
   const handleOnSubmit = async () => {
     const recipeItem = {
@@ -121,9 +134,11 @@ const AddRecipeForm = () => {
       recipeImage: imgURL,
       category: category.value,
       cockingTime: cookingTimeRecipe.value,
-      ingredients: userIngredientsList.map(e => { return { [`${e.id}`]: `${e.unitCount} ${e.unit}` } }),
+      ingredients: userIngredientsList.map(e => {
+        return { [`${e.id}`]: `${e.unitCount} ${e.unit}` };
+      }),
       preparation: recipePreparation,
-    }
+    };
 
     // console.log(recipeItem);
     // resetForm();
@@ -135,63 +150,73 @@ const AddRecipeForm = () => {
           padding: '10px',
         });
         resetForm();
-      }
-      );
+      });
     } catch (error) {
-       Notify.failure('Error database connection!', {
-            fontSize: '16px',
-            width: '350px',
-            padding: '10px',
-          });
+      Notify.failure('Error database connection!', {
+        fontSize: '16px',
+        width: '350px',
+        padding: '10px',
+      });
     }
-  }
+  };
 
-  const handleOnImgSelect = async (e) => {
+  const handleOnImgSelect = async e => {
     const localFile = e.target.files[0];
     const newURL = await addRecipeImg(localFile);
     if (newURL) {
       setImageURL(newURL);
       return;
-    } 
+    }
     setImageURL('');
-  }
+  };
 
   return (
     <>
-    <WrapperPage>
+      <WrapperPage>
         <Formik initialValues={initialValues} onSubmit={handleOnSubmit}>
-        <StyledForm autoComplete="off">
-          <RecipeDescriptionFields
-            dataField={{ imgURL, itemTitleRecipe, aboutRecipe, category, cookingTimeRecipe }}
-            handleOnImgSelect={handleOnImgSelect}
-            handleOnTitleRecipe={setItemTitleRecipe}
-            handleOnAboutRecipe={setAboutRecipe}
-            handleOnCategory={setCategory}
-            handleOnCookingTimeRecipe={setCookingTimeRecipe} />
-          <RecipeIngredientsFields
-            userList={{ userIngredientsList }}
-            handleIncIngredient={handleIncIngredient}
-            handleDecIngredient={handleDecIngredient}
-            handleOnDeleteIngredient={handleOnDeleteIngredient}
-            handleOnChangeIngName={handleOnChangeIngName}
-            handleOnChangeIngUnit={handleOnChangeIngUnit}
-            handleOnChangeUnitCount={handleOnChangeUnitCount}
-          />
-          <RecipePreparationFields dataField={recipePreparation} handleOnRecipePreperation={setRecipePreperation} />
-          <WrapperSubmitButton><SearchBlackBtn type="submit">Add</SearchBlackBtn></WrapperSubmitButton>
-          
-        </StyledForm>    
-      </Formik>
-      <WrapperPopularRecipe>
+          <StyledForm autoComplete="off">
+            <RecipeDescriptionFields
+              dataField={{
+                imgURL,
+                itemTitleRecipe,
+                aboutRecipe,
+                category,
+                cookingTimeRecipe,
+              }}
+              handleOnImgSelect={handleOnImgSelect}
+              handleOnTitleRecipe={setItemTitleRecipe}
+              handleOnAboutRecipe={setAboutRecipe}
+              handleOnCategory={setCategory}
+              handleOnCookingTimeRecipe={setCookingTimeRecipe}
+            />
+            <RecipeIngredientsFields
+              userList={{ userIngredientsList }}
+              handleIncIngredient={handleIncIngredient}
+              handleDecIngredient={handleDecIngredient}
+              handleOnDeleteIngredient={handleOnDeleteIngredient}
+              handleOnChangeIngName={handleOnChangeIngName}
+              handleOnChangeIngUnit={handleOnChangeIngUnit}
+              handleOnChangeUnitCount={handleOnChangeUnitCount}
+            />
+            <RecipePreparationFields
+              dataField={recipePreparation}
+              handleOnRecipePreperation={setRecipePreperation}
+            />
+            <WrapperSubmitButton>
+              <SearchBlackBtn type="submit">Add</SearchBlackBtn>
+            </WrapperSubmitButton>
+          </StyledForm>
+        </Formik>
+        <WrapperPopularRecipe>
           <WrapperSocialLinks>
             <TitleStyled>Follow us</TitleStyled>
-              <SocialLinks />
+            <SocialLinks />
           </WrapperSocialLinks>
           <PopularRecipe popularRecipeList={popularRecipeList} />
-      </WrapperPopularRecipe>        
-    </WrapperPage>
+        </WrapperPopularRecipe>
+      </WrapperPage>
     </>
-  )
-}
+  );
+};
 
 export default AddRecipeForm;
