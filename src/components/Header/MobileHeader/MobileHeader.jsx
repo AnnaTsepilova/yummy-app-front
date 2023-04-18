2d90cfdac58402e7be77b7afccb66bae0deed900
Anna Tsepilova <virgo.tsav@gmail.com>
2023-04-18 22:27:34 +0300

    fix scroll

--- src/components/Header/MobileHeader/MobileHeader.jsx
+++ src/components/Header/MobileHeader/MobileHeader.jsx
@@ -16,7 +16,8 @@ const MobileHeader = () => {
   const openNav = () => {
     setIsOpen(!isOpen);
     !isOpen
-      ? (document.body.style.overflow = 'hidden')
+      ? // ? (document.body.style.overflow = 'hidden')
+        (document.body.style.overflow = 'scroll')
       : (document.body.style.overflow = 'scroll');
   };
 
